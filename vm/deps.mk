5d9865429e83df8e469bfef577a788d6320c2674
Janavi <jkumar@umail.ucsb.edu>
2018-06-02 12:18:34 -0700

    Created repo

---
+++ vm/deps.mk
@@ -0,0 +1,236 @@
+main.o: ../threads/main.cc ../threads/copyright.h ../threads/utility.h \
+  ../threads/bool.h ../machine/sysdep.h ../bin/copyright.h \
+  ../threads/system.h ../threads/thread.h ../machine/machine.h \
+  ../machine/translate.h ../machine/disk.h ../userprog/addrspace.h \
+  ../filesys/filesys.h ../filesys/openfile.h ../userprog/pcb.h \
+  ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+list.o: ../threads/list.cc ../threads/copyright.h ../threads/list.h \
+  ../threads/utility.h ../threads/bool.h ../machine/sysdep.h \
+  ../bin/copyright.h
+scheduler.o: ../threads/scheduler.cc ../threads/copyright.h \
+  ../threads/scheduler.h ../threads/list.h ../threads/utility.h \
+  ../threads/bool.h ../machine/sysdep.h ../bin/copyright.h \
+  ../threads/thread.h ../machine/machine.h ../machine/translate.h \
+  ../machine/disk.h ../userprog/addrspace.h ../filesys/filesys.h \
+  ../filesys/openfile.h ../userprog/pcb.h ../userprog/bitmap.h \
+  ../userprog/useropenfile.h ../userprog/memorymanager.h \
+  ../threads/system.h ../machine/interrupt.h ../machine/stats.h \
+  ../machine/timer.h ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+synch.o: ../threads/synch.cc ../threads/copyright.h ../threads/synch.h \
+  ../threads/thread.h ../threads/utility.h ../threads/bool.h \
+  ../machine/sysdep.h ../bin/copyright.h ../machine/machine.h \
+  ../machine/translate.h ../machine/disk.h ../userprog/addrspace.h \
+  ../filesys/filesys.h ../filesys/openfile.h ../userprog/pcb.h \
+  ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/list.h ../threads/system.h \
+  ../threads/scheduler.h ../machine/interrupt.h ../machine/stats.h \
+  ../machine/timer.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+synchlist.o: ../threads/synchlist.cc ../threads/copyright.h \
+  ../threads/synchlist.h ../threads/list.h ../threads/utility.h \
+  ../threads/bool.h ../machine/sysdep.h ../bin/copyright.h \
+  ../threads/synch.h ../threads/thread.h ../machine/machine.h \
+  ../machine/translate.h ../machine/disk.h ../userprog/addrspace.h \
+  ../filesys/filesys.h ../filesys/openfile.h ../userprog/pcb.h \
+  ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h
+system.o: ../threads/system.cc ../threads/copyright.h ../threads/system.h \
+  ../threads/utility.h ../threads/bool.h ../machine/sysdep.h \
+  ../bin/copyright.h ../threads/thread.h ../machine/machine.h \
+  ../machine/translate.h ../machine/disk.h ../userprog/addrspace.h \
+  ../filesys/filesys.h ../filesys/openfile.h ../userprog/pcb.h \
+  ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+thread.o: ../threads/thread.cc ../threads/copyright.h ../threads/thread.h \
+  ../threads/utility.h ../threads/bool.h ../machine/sysdep.h \
+  ../bin/copyright.h ../machine/machine.h ../machine/translate.h \
+  ../machine/disk.h ../userprog/addrspace.h ../filesys/filesys.h \
+  ../filesys/openfile.h ../userprog/pcb.h ../userprog/bitmap.h \
+  ../userprog/useropenfile.h ../userprog/memorymanager.h \
+  ../threads/switch.h ../threads/synch.h ../threads/list.h \
+  ../threads/system.h ../threads/scheduler.h ../machine/interrupt.h \
+  ../machine/stats.h ../machine/timer.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+utility.o: ../threads/utility.cc ../threads/copyright.h \
+  ../threads/utility.h ../threads/bool.h ../machine/sysdep.h \
+  ../bin/copyright.h ../threads/stdarg.h
+threadtest.o: ../threads/threadtest.cc ../threads/copyright.h \
+  ../threads/system.h ../threads/utility.h ../threads/bool.h \
+  ../machine/sysdep.h ../bin/copyright.h ../threads/thread.h \
+  ../machine/machine.h ../machine/translate.h ../machine/disk.h \
+  ../userprog/addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
+  ../userprog/pcb.h ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+interrupt.o: ../machine/interrupt.cc ../bin/copyright.h \
+  ../machine/interrupt.h ../threads/list.h ../threads/copyright.h \
+  ../threads/utility.h ../threads/bool.h ../machine/sysdep.h \
+  ../threads/system.h ../threads/thread.h ../machine/machine.h \
+  ../machine/translate.h ../machine/disk.h ../userprog/addrspace.h \
+  ../filesys/filesys.h ../filesys/openfile.h ../userprog/pcb.h \
+  ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../machine/stats.h \
+  ../machine/timer.h ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+sysdep.o: ../machine/sysdep.cc ../bin/copyright.h ../machine/interrupt.h \
+  ../threads/list.h ../threads/copyright.h ../threads/utility.h \
+  ../threads/bool.h ../machine/sysdep.h ../threads/system.h \
+  ../threads/thread.h ../machine/machine.h ../machine/translate.h \
+  ../machine/disk.h ../userprog/addrspace.h ../filesys/filesys.h \
+  ../filesys/openfile.h ../userprog/pcb.h ../userprog/bitmap.h \
+  ../userprog/useropenfile.h ../userprog/memorymanager.h \
+  ../threads/scheduler.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+stats.o: ../machine/stats.cc ../bin/copyright.h ../threads/utility.h \
+  ../threads/copyright.h ../threads/bool.h ../machine/sysdep.h \
+  ../machine/stats.h
+timer.o: ../machine/timer.cc ../bin/copyright.h ../machine/timer.h \
+  ../threads/utility.h ../threads/copyright.h ../threads/bool.h \
+  ../machine/sysdep.h ../threads/system.h ../threads/thread.h \
+  ../machine/machine.h ../machine/translate.h ../machine/disk.h \
+  ../userprog/addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
+  ../userprog/pcb.h ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../threads/synch.h \
+  ../userprog/processmanager.h ../userprog/openfilemanager.h \
+  ../userprog/sysopenfile.h ../vm/virtualmemorymanager.h
+addrspace.o: ../userprog/addrspace.cc ../bin/copyright.h \
+  ../threads/system.h ../threads/copyright.h ../threads/utility.h \
+  ../threads/bool.h ../machine/sysdep.h ../threads/thread.h \
+  ../machine/machine.h ../machine/translate.h ../machine/disk.h \
+  ../userprog/addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
+  ../userprog/pcb.h ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h ../bin/noff.h
+bitmap.o: ../userprog/bitmap.cc ../bin/copyright.h ../userprog/bitmap.h \
+  ../threads/utility.h ../threads/copyright.h ../threads/bool.h \
+  ../machine/sysdep.h ../filesys/openfile.h
+exception.o: ../userprog/exception.cc ../bin/copyright.h \
+  ../threads/system.h ../threads/copyright.h ../threads/utility.h \
+  ../threads/bool.h ../machine/sysdep.h ../threads/thread.h \
+  ../machine/machine.h ../machine/translate.h ../machine/disk.h \
+  ../userprog/addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
+  ../userprog/pcb.h ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h ../userprog/syscall.h
+progtest.o: ../userprog/progtest.cc ../bin/copyright.h \
+  ../threads/system.h ../threads/copyright.h ../threads/utility.h \
+  ../threads/bool.h ../machine/sysdep.h ../threads/thread.h \
+  ../machine/machine.h ../machine/translate.h ../machine/disk.h \
+  ../userprog/addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
+  ../userprog/pcb.h ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h ../machine/console.h
+console.o: ../machine/console.cc ../bin/copyright.h ../machine/console.h \
+  ../threads/utility.h ../threads/copyright.h ../threads/bool.h \
+  ../machine/sysdep.h ../threads/system.h ../threads/thread.h \
+  ../machine/machine.h ../machine/translate.h ../machine/disk.h \
+  ../userprog/addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
+  ../userprog/pcb.h ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+machine.o: ../machine/machine.cc ../bin/copyright.h ../machine/machine.h \
+  ../threads/utility.h ../threads/copyright.h ../threads/bool.h \
+  ../machine/sysdep.h ../machine/translate.h ../machine/disk.h \
+  ../threads/system.h ../threads/thread.h ../userprog/addrspace.h \
+  ../filesys/filesys.h ../filesys/openfile.h ../userprog/pcb.h \
+  ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+mipssim.o: ../machine/mipssim.cc ../bin/copyright.h ../machine/machine.h \
+  ../threads/utility.h ../threads/copyright.h ../threads/bool.h \
+  ../machine/sysdep.h ../machine/translate.h ../machine/disk.h \
+  ../machine/mipssim.h ../threads/system.h ../threads/thread.h \
+  ../userprog/addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
+  ../userprog/pcb.h ../userprog/bitmap.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+translate.o: ../machine/translate.cc ../bin/copyright.h \
+  ../machine/machine.h ../threads/utility.h ../threads/copyright.h \
+  ../threads/bool.h ../machine/sysdep.h ../machine/translate.h \
+  ../machine/disk.h ../userprog/addrspace.h ../filesys/filesys.h \
+  ../filesys/openfile.h ../userprog/pcb.h ../userprog/bitmap.h \
+  ../userprog/useropenfile.h ../userprog/memorymanager.h \
+  ../threads/system.h ../threads/thread.h ../threads/scheduler.h \
+  ../threads/list.h ../machine/interrupt.h ../machine/stats.h \
+  ../machine/timer.h ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../vm/virtualmemorymanager.h
+memorymanager.o: ../userprog/memorymanager.cc ../userprog/memorymanager.h \
+  ../userprog/bitmap.h ../bin/copyright.h ../threads/utility.h \
+  ../threads/copyright.h ../threads/bool.h ../machine/sysdep.h \
+  ../filesys/openfile.h ../machine/machine.h ../machine/translate.h \
+  ../machine/disk.h
+processmanager.o: ../userprog/processmanager.cc \
+  ../userprog/processmanager.h ../userprog/pcb.h ../userprog/bitmap.h \
+  ../bin/copyright.h ../threads/utility.h ../threads/copyright.h \
+  ../threads/bool.h ../machine/sysdep.h ../filesys/openfile.h \
+  ../userprog/useropenfile.h ../threads/synch.h ../threads/thread.h \
+  ../machine/machine.h ../machine/translate.h ../machine/disk.h \
+  ../userprog/addrspace.h ../filesys/filesys.h \
+  ../userprog/memorymanager.h ../threads/list.h ../threads/system.h \
+  ../threads/scheduler.h ../machine/interrupt.h ../machine/stats.h \
+  ../machine/timer.h ../userprog/openfilemanager.h \
+  ../userprog/sysopenfile.h ../vm/virtualmemorymanager.h
+pcb.o: ../userprog/pcb.cc ../userprog/pcb.h ../userprog/bitmap.h \
+  ../bin/copyright.h ../threads/utility.h ../threads/copyright.h \
+  ../threads/bool.h ../machine/sysdep.h ../filesys/openfile.h \
+  ../userprog/useropenfile.h
+sysopenfile.o: ../userprog/sysopenfile.cc ../userprog/sysopenfile.h \
+  ../filesys/filesys.h ../bin/copyright.h ../filesys/openfile.h \
+  ../threads/utility.h ../threads/copyright.h ../threads/bool.h \
+  ../machine/sysdep.h
+openfilemanager.o: ../userprog/openfilemanager.cc \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h \
+  ../filesys/filesys.h ../bin/copyright.h ../filesys/openfile.h \
+  ../threads/utility.h ../threads/copyright.h ../threads/bool.h \
+  ../machine/sysdep.h ../userprog/bitmap.h
+useropenfile.o: ../userprog/useropenfile.cc ../userprog/useropenfile.h
+virtualmemorymanager.o: ../vm/virtualmemorymanager.cc \
+  ../machine/machine.h ../bin/copyright.h ../threads/utility.h \
+  ../threads/copyright.h ../threads/bool.h ../machine/sysdep.h \
+  ../machine/translate.h ../machine/disk.h ../vm/virtualmemorymanager.h \
+  ../userprog/bitmap.h ../filesys/openfile.h ../threads/system.h \
+  ../threads/thread.h ../userprog/addrspace.h ../filesys/filesys.h \
+  ../userprog/pcb.h ../userprog/useropenfile.h \
+  ../userprog/memorymanager.h ../threads/scheduler.h ../threads/list.h \
+  ../machine/interrupt.h ../machine/stats.h ../machine/timer.h \
+  ../threads/synch.h ../userprog/processmanager.h \
+  ../userprog/openfilemanager.h ../userprog/sysopenfile.h
---
+++ vm/virtualmemorymanager.cc
@@ -0,0 +1,212 @@
+/*
+ * VirtualMemoryManager implementation
+ *
+ * Used to facilitate demand paging through providing a means by which page
+ * faults can be handled and pages loaded from and stored to disk.
+*/
+
+#include <stdlib.h>
+#include <machine.h>
+#include "virtualmemorymanager.h"
+#include "system.h"
+
+VirtualMemoryManager::VirtualMemoryManager()
+{
+    fileSystem->Create(SWAP_FILENAME, SWAP_SECTOR_SIZE * SWAP_SECTORS);
+    swapFile = fileSystem->Open(SWAP_FILENAME);
+
+    swapSectorMap = new BitMap(SWAP_SECTORS);
+    physicalMemoryInfo = new FrameInfo[NumPhysPages];
+    for(int i = 0; i < NumPhysPages; i++){
+        //go through the physical memory and set each AddrSpace page to null
+        physicalMemoryInfo[i].space = NULL;
+    }
+
+    //swapSpaceInfo = new SwapSectorInfo[SWAP_SECTORS];
+
+    nextVictim = 0;
+}
+
+VirtualMemoryManager::~VirtualMemoryManager()
+{
+    fileSystem->Remove(SWAP_FILENAME);
+    delete swapFile;
+    delete [] physicalMemoryInfo;
+    //delete [] swapSpaceInfo;
+}
+
+int VirtualMemoryManager::allocSwapSector()
+{
+    int location = swapSectorMap->Find() * PageSize; // also marks the bit
+    return location;
+}
+/*
+SwapSectorInfo * VirtualMemoryManager::getSwapSectorInfo(int index)
+{
+    return swapSpaceInfo + index;
+    
+}
+*/
+void VirtualMemoryManager::writeToSwap(char *page, int pageSize,
+                                       int backStoreLoc)
+{
+    swapFile->WriteAt(page, pageSize, backStoreLoc);
+}
+
+/*
+ * Page replacement with  the second chance algorithm
+ */
+void VirtualMemoryManager::swapPageIn(int virtAddr)
+{
+
+        TranslationEntry* currPageEntry;
+        
+        //create a stack frame that holds information about the pages in the memory manager
+        FrameInfo* physicalPageInfo;
+        physicalPageInfo = nextVictim + physicalMemoryInfo;
+        //create an object to hold the translation between the old physical page and the its virtual counterpart
+        TranslationEntry* prevPageEntry;
+
+        //set up a sort of circular queue; if all the pages have their
+        //referenced bit set, on the second encounter of the first page
+        //in the list, the page will be swapped out, as it will
+        //have its reference bit cleared. if all pages have it cleared
+        //implement FIFO scheme; if no more space, return
+
+        if(nextVictim>= NumPhysPages) {//no more space available
+                fprintf(stderr, "Fatal error: No more space available\n");
+                exit(1);
+                return;
+        }
+
+        //if there is space, find the next open place from front of the queue
+        while(physicalPageInfo->space != NULL){
+            //if the reference bit is set for current page in stack then clear it
+            if(getPageTableEntry(physicalPageInfo)->use = true){
+                getPageTableEntry(physicalPageInfo)->use = false;
+                //increment the spot you're looking at
+                nextVictim++;
+                nextVictim %= NumPhysPages;
+                //in the stack, move to the next location (remember to % NumPhysPages
+                //as this works in a cyclical queue and needs to wrap around
+                physicalPageInfo = physicalMemoryInfo + (nextVictim);
+            }
+            //if the next reference bit is not set, then break out, as you've found
+            //the spot
+            if(getPageTableEntry(physicalPageInfo)->use = false){
+                break;
+            }
+        }
+
+        //now, if there is space in the stack frame for physical pages
+        //find the victim page and swap
+        if(physicalPageInfo->space != NULL){
+            //set the old page 
+            prevPageEntry = getPageTableEntry(physicalPageInfo);
+            bool d = prevPageEntry->dirty;
+            if(d == true){
+                char* mmMemory = machine->mainMemory;
+                int p = prevPageEntry->physicalPage * PageSize;
+                int index = physicalPageInfo->pageTableIndex;
+                TranslationEntry* sw = physicalPageInfo->space->getPageTableEntry(index);
+                writeToSwap(mmMemory + p, PageSize, sw->locationOnDisk);
+                prevPageEntry->dirty = false;
+            }
+
+            //swap the old page with the current page
+            physicalPageInfo->space = currentThread->space;
+            int newIndex = virtAddr/PageSize;
+            physicalPageInfo->pageTableIndex = newIndex;
+            currPageEntry = getPageTableEntry(physicalPageInfo);
+            currPageEntry->physicalPage = prevPageEntry->physicalPage;
+            loadPageToCurrVictim(virtAddr);
+            prevPageEntry->valid = false;
+        }
+
+
+        //if there is no space, however, make room, then increment nextVictim
+        //to move to the correct point in the stack for the following slot search
+        else{
+            //point the stack to a new physical page structure
+            *physicalPageInfo = FrameInfo();
+            int newIndex = virtAddr/PageSize;
+            physicalPageInfo->pageTableIndex = newIndex;
+            physicalPageInfo->space = currentThread->space;
+
+            currPageEntry = getPageTableEntry(physicalPageInfo);
+            //access the page number in real memory and set 
+            currPageEntry->physicalPage = memoryManager->getPage();
+            loadPageToCurrVictim(virtAddr);
+
+        }
+    
+        nextVictim++;
+        nextVictim = nextVictim % NumPhysPages;
+
+        
+}
+
+
+/*
+ * Cleanup the physical memory allocated to a given address space after its 
+ * destructor invokes.
+*/
+void VirtualMemoryManager::releasePages(AddrSpace* space)
+{
+    for (int i = 0; i < space->getNumPages(); i++)
+    {
+        TranslationEntry* currPage = space->getPageTableEntry(i);
+    //  int swapSpaceIndex = (currPage->locationOnDisk) / PageSize;
+ //     SwapSectorInfo * swapPageInfo = swapSpaceInfo + swapSpaceIndex;
+//      swapPageInfo->removePage(currPage);
+      //swapPageInfo->pageTableEntry = NULL;
+
+        if (currPage->valid == TRUE)
+        {
+            //int currPID = currPage->space->getPCB()->getPID();
+            int currPID = space->getPCB()->getPID();
+            DEBUG('v', "E %d: %d\n", currPID, currPage->virtualPage);
+            memoryManager->clearPage(currPage->physicalPage);
+            physicalMemoryInfo[currPage->physicalPage].space = NULL; 
+        }
+        swapSectorMap->Clear((currPage->locationOnDisk) / PageSize);
+    }
+}
+
+/*
+ * After selecting a slot of physical memory as a victim and taking care of
+ * synchronizing the data if needed, we load the faulting page into memory.
+*/
+void VirtualMemoryManager::loadPageToCurrVictim(int virtAddr)
+{
+    int pageTableIndex = virtAddr / PageSize;
+    TranslationEntry* page = currentThread->space->getPageTableEntry(pageTableIndex);
+    char* physMemLoc = machine->mainMemory + page->physicalPage * PageSize;
+    int swapSpaceLoc = page->locationOnDisk;
+    swapFile->ReadAt(physMemLoc, PageSize, swapSpaceLoc);
+
+  //  int swapSpaceIndex = swapSpaceLoc / PageSize;
+ //   SwapSectorInfo * swapPageInfo = swapSpaceInfo + swapSpaceIndex;
+    page->valid = TRUE;
+//    swapPageInfo->setValidBit(TRUE);
+//    swapPageInfo->setPhysMemPageNum(page->physicalPage);
+}
+
+/*
+ * Helper function for the second chance page replacement that retrieves the physical page
+ * which corresponds to the given physical memory page information that the
+ * VirtualMemoryManager maintains.
+ * This return page table entry corresponding to a physical page
+ */
+TranslationEntry* VirtualMemoryManager::getPageTableEntry(FrameInfo * physPageInfo)
+{
+    TranslationEntry* page = physPageInfo->space->getPageTableEntry(physPageInfo->pageTableIndex);
+    return page;
+}
+
+void VirtualMemoryManager::copySwapSector(int to, int from)
+{
+    char sectorBuf[SectorSize];
+    swapFile->ReadAt(sectorBuf, SWAP_SECTOR_SIZE, from);
+    swapFile->WriteAt(sectorBuf, SWAP_SECTOR_SIZE, to);
+}
